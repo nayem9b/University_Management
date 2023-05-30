bfcf9389deb15a7f636585ef0285b0683921887c
nayem9b <razwanniam1@gmail.com>
2023-05-30 22:24:05 +0600

    app setup

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// testing
+app.get("/", async (req: Request, res: Response) => {
+  res.send("App is working");
+});
