02f8c2da484c4be6b917ff9937fe5df6eb246a22
nayem9b <razwanniam1@gmail.com>
2023-06-01 02:05:54 +0600

    linter added

---
+++ src/app/modules/users/users.service.ts
@@ -0,0 +1,10 @@
+import { IUser } from "./users.interface";
+import { User } from "./users.model";
+
+export const createUser = async (user: IUser): Promise<IUser | null> => {
+  const createdUser = await User.create(user);
+  if (!createdUser) {
+    throw new Error("User not created");
+  }
+  return createdUser;
+};
